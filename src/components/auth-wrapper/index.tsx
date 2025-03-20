bf4de1adb801d2ae83ac2c3ecb892ba66d03dac1
GabrielDeFreitas <gabrielfreitas.ilustra@gmail.com>
2025-03-19 23:22:08 -0300

    fix: padding responsive

--- src/components/auth-wrapper/index.tsx
+++ src/components/auth-wrapper/index.tsx
@@ -1,5 +1,5 @@
 import { AuthWrapperProps } from './types'
 
 export default function AuthWrapper({ children }: AuthWrapperProps) {
-  return <div className="min-h-screen flex items-center justify-center bg-gray-100 flex-col gap-4">{children}</div>
+  return <div className="min-h-screen flex items-center justify-center bg-gray-100 flex-col gap-4 p-4">{children}</div>
 }
